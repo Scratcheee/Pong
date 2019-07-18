8afd9cb885e898b34bd6522c2be9683ad9bad529
Lawrence Kaul <kaul.lawrence@gmail.com>
2019-07-18 02:41:28 -0500

    Created PongGame constructor and started coding PongGame class

--- app/src/main/java/com/lawrencekaul/pong/MainActivity.java
+++ app/src/main/java/com/lawrencekaul/pong/MainActivity.java
@@ -2,13 +2,39 @@ package com.lawrencekaul.pong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.PointerIcon;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PongGame mPongGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        //More code here later in the chapter
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        //More code here later in the chapter
     }
 }
--- app/src/main/java/com/lawrencekaul/pong/PongGame.java
+++ app/src/main/java/com/lawrencekaul/pong/PongGame.java
@@ -1,4 +1,19 @@
 package com.lawrencekaul.pong;
 
-class PongGame {
+import android.content.Context;
+import android.view.SurfaceView;
+
+class PongGame extends SurfaceView {
+
+//    The PongGame constructor
+//     Called when this line:
+//     mPongGame = new PongGame (this, size.x, size.y);
+//     is executed from PongActivity
+
+    public PongGame (Context context, int x, int y){
+//        Super... calls the parent of the class
+//        constructor of SurfaceView
+//        provided by Android
+        super(context);
+    }
 }
